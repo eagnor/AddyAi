2ba1338daf90cd6e19dba94283902493d7c2f682
eagnor <130266294+eagnor@users.noreply.github.com>
2026-04-19 07:11:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/identify', async (req, res) => {
